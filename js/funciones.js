448effd6249a20f5baebbae99b19466c4268f498
Marcepe2022 <113952948+Marcepe2022@users.noreply.github.com>
2022-10-09 17:10:01 -0300

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,113 @@
+
+ // Función para validar un Apellido o Nombre 
+  function validateApellido(apellido)
+  {
+ //Creamos un objeto 
+ object=document.getElementById(apellido);
+ valueForm=object.value;
+  //  Patron para el apellido 
+  var patron=/^[a-z A-z]{2,16}$/;
+    if(valueForm.search(patron)==0)
+	{
+	 //Apellido correcto Con color azul
+	object.style.color="#0033CC";
+	 return;
+	 }
+	  //Apellido incorrecto Con rojo
+	 object.style.color="#990000";
+	 alert('No puede Escribir Números u otros Caractéres que no sean letras');
+ 
+   } 
+
+   function validarForm(){
+    
+    var verificar = true;
+    
+      var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+      var expRegApellidos=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var expRegCorreo=/^[\w-\.]+@([\w-])+\.)+[\w-]{2,4}$/; 
+    
+    
+         var formulario = document.getElementById("contacto-frm");
+         var nombre = document.getElementById("nombre");
+         var apellidos = document.getElementById("apellidos");
+         var correo = document.getElementById("correo");
+         var asunto = document.getElementById("asunto");
+         var comentarios = document.getElementById("comentarios");
+    //Campo nombre
+         if(!nombre.value)
+         {
+          alert("El campo nombre es requerido");
+          nombre.focus();
+          verificar==false;
+         }
+         else if (!expRegNombre.exec(nombre.value))
+         {
+            alert("El campo nombre admite letras y espacios.")
+            nombre.focus();
+            verificar=false;
+    
+         }
+    //CAmpo apellido
+    
+          else if(!apellidos.value)
+         {
+          alert("El campo apellidos es requerido");
+          apellidos.focus();
+          verificar=false;
+         }
+         else if(!expRegApellidos.exec(apellidos.value))
+         {
+           alert("El campo apellidos admite letras y espacios.")
+           apellidos.focus();
+           verificar=false;
+         }
+    
+    
+    //campo email
+          else if(!correo.value)
+         {
+          alert("El campo correo es requerido");
+          correo.focus();
+          verificar=false;
+         }
+         else if(!expRegCorreo.exec(correo.value))
+         {
+           alert("El campo nombre admite letras y espacios.")
+           correo.focus();
+           verificar=false;
+         }
+    // campo asunto
+           else if(!asunto.value)
+         {
+          alert("El campo asunto es requerido");
+          nombre.focus();
+          verificar=false;
+         }
+    //campo comentarios
+          else if(!comentarios.value)
+         {
+          alert("El campo comentarios es requerido");
+          comentarios.focus();
+          verificar=false;
+         }
+    
+        if(verificar)
+        {
+          alert("Se ha enviado el formulario");
+         document.contacto_frm.submit();
+        }
+    
+    
+    
+    alert("Enviando");
+     document.contacto_frm.submit();
+    
+      }
+      // asociacion del boton enviar con el formulario en html//
+      window.onload = function()
+      { 
+        var botonEnviar;
+        botonEnviar = document.contacto_frm.enviar_btn;
+        botonEnviar.onclick = validarForm;
+      }
